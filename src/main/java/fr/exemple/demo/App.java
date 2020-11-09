f4b652c9bbd16a9d3e3084077e68987a882f2aa8
Grafikart <contact@grafikart.fr>
2020-11-09 14:00:35 +0100

    mon premier commit

---
+++ src/main/java/fr/exemple/demo/App.java
@@ -0,0 +1,13 @@
+package fr.exemple.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
